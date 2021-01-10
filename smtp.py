1997418d9d25c1d5b07dfd893a6db45cba86d46d
Golden Owl <74717043+Owl-planet@users.noreply.github.com>
2021-01-10 17:08:25 +0300

    Create smtp.py

---
+++ smtp.py
@@ -0,0 +1,12 @@
+import smtplib
+#################################################################################################################
+def send_mail(email,password,message):                                                                          #
+    email_server = smtplib.SMTP("smtp.gmail.com",587) ## Server Address, Port.                                  #
+    email_server.starttls() ## Connect server.                                                                  #
+    email_server.login(email,password) ## Login with 1 par , 2 par.                                             #
+    email_server.sendmail(email,email,message) ## Sender, Target of sender. => Mail message.                    #
+    email_server.quit() ## Quit the server.                                                                     #
+#################################################################################################################
+## <-------------------- Coded By SBDH-cosmin (Owl-planet) https://github.com/Owl-planet --------------------> ##
+#################################################################################################################
+send_mail("example@gmail.com" , "password123" , "test")
